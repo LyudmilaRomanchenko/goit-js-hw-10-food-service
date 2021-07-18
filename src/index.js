5b8c06344cf49953a5d9ba193ff2a51879bf15d1
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-07-18 23:14:17 +0300

    Добавляет изменение темы

--- src/index.js
+++ src/index.js
@@ -29,22 +29,30 @@ function createMenuList(menu) {
 
 // Реализуем мозможность выбора темы
 
-const slectCheckbox = document.querySelector('.theme-switch__toggle');
-console.log(slectCheckbox);
+const selectCheckbox = document.querySelector('.theme-switch__toggle');
+console.log(selectCheckbox);
 
 const getBody = document.querySelector('body');
 console.log(getBody);
 
-slectCheckbox.addEventListener('change', onSelectThemes);
+selectCheckbox.addEventListener('change', onSelectThemes);
+
 
 function onSelectThemes(event) {
-    if (slectCheckbox.checked) {
+    if (selectCheckbox.checked) {
         getBody.classList.remove(Theme.LIGHT);
         getBody.classList.add(Theme.DARK);
         const darkTheme = getBody.getAttribute('class');
         console.log(darkTheme);
-        slectCheckbox.setAttribute('checked', 'true');
+        
         localStorage.setItem('theme', darkTheme);
+        //console.log(localStorage.getItem('theme'));
+        if (localStorage.getItem('theme')) {
+            selectCheckbox.setAttribute('checked', 'true');
+        }
+       
+        //save();
+       
     }
     else {
         getBody.classList.remove(Theme.DARK);
@@ -52,6 +60,19 @@ function onSelectThemes(event) {
         const lightTheme = getBody.getAttribute('class');
         console.log(lightTheme);
         localStorage.setItem('theme', lightTheme);
+        
     }
 
 }
+// function saveCheckboxstatus() {
+    
+        
+// }
+
+// function save(){
+    
+//     if(selectCheckbox.checked) {
+//         localStorage.setItem('checkbox1zaal1', true);
+//     }
+// }
+//localStorage.removeItem('checkbox1zaal1');
